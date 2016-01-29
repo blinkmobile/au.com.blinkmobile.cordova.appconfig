2b187cedbcce8410a9eba553e40237cdbcbc2fbf
Shane MacPhillamy <shane@blinkmobile.com.au>
2016-01-29 16:21:12 +1100

    Module name rationalisation

--- www/airwatch-ace.js
+++ www/airwatch-ace.js
@@ -34,33 +34,21 @@ channel.waitForInitialization('onCordovaInfoReady');
  * phone, etc.
  * @constructor
  */
-function Device() {
+function AirwatchAce() {
     this.available = false;
-    this.platform = null;
     this.version = null;
-    this.uuid = null;
     this.cordova = null;
-    this.model = null;
-    this.manufacturer = null;
     this.isVirtual = null;
-    this.serial = null;
 
     var me = this;
 
     channel.onCordovaReady.subscribe(function() {
         me.getInfo(function(info) {
-            //ignoring info.cordova returning from native, we should use value from cordova.version defined in cordova.js
-            //TODO: CB-5105 native implementations should not return info.cordova
             var buildLabel = cordova.version;
             me.available = true;
-            me.platform = info.platform;
             me.version = info.version;
-            me.uuid = info.uuid;
             me.cordova = buildLabel;
-            me.model = info.model;
             me.isVirtual = info.isVirtual;
-            me.manufacturer = info.manufacturer || 'unknown';
-            me.serial = info.serial || 'unknown';
             channel.onCordovaInfoReady.fire();
         },function(e) {
             me.available = false;
@@ -70,14 +58,14 @@ function Device() {
 }
 
 /**
- * Get device info
+ * Get Airwatch ACE info
  *
  * @param {Function} successCallback The function to call when the heading data is available
  * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
  */
-Device.prototype.getInfo = function(successCallback, errorCallback) {
-    argscheck.checkArgs('fF', 'Device.getInfo', arguments);
-    exec(successCallback, errorCallback, "Device", "getDeviceInfo", []);
+AirwatchAce.prototype.getInfo = function(successCallback, errorCallback) {
+    argscheck.checkArgs('fF', 'AirwatchAce.getInfo', arguments);
+    exec(successCallback, errorCallback, "AirwatchAce", "getDeviceInfo", []);
 };
 
-module.exports = new Device();
+module.exports = new AirwatchAce();
