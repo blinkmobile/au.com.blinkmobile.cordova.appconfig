d6b001e45b8ad3bca89a8d77edc635743f37b9e4
Shane MacPhillamy <shane@blinkmobile.com.au>
2016-03-18 11:39:57 +1100

    Rename to AppConfig

---
+++ www/appconfig.js
@@ -0,0 +1,72 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+"use strict";
+
+var argscheck = require('cordova/argscheck'),
+    channel = require('cordova/channel'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec'),
+    cordova = require('cordova');
+               
+channel.createSticky('onAirwatchAceReady');
+// Tell cordova channel to wait on the AirwatchAceReady event
+channel.waitForInitialization('onAirwatchAceReady');
+
+/**
+ * This represents the mobile device, and provides properties for inspecting the model, version, UUID of the
+ * phone, etc.
+ * @constructor
+ */
+function AirwatchAce() {
+    this.available = false;
+    this.version = null;
+    this.cordova = null;
+    this.isVirtual = null;
+
+    var me = this;
+
+    channel.onCordovaReady.subscribe(function() {
+        me.getInfo(function(info) {
+            var buildLabel = cordova.version;
+            me.available = true;
+            me.version = info.version;
+            me.cordova = buildLabel;
+            me.isVirtual = info.isVirtual;
+            channel.onAirwatchAceReady.fire();
+        },function(e) {
+            me.available = false;
+            utils.alert("[ERROR] Error initializing Cordova: " + e);
+        });
+    });
+}
+
+/**
+ * Get Airwatch info
+ *
+ * @param {Function} successCallback The function to call when the heading data is available
+ * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
+ */
+AirwatchAce.prototype.getInfo = function(successCallback, errorCallback) {
+    argscheck.checkArgs('fF', 'AirwatchAce.getInfo', arguments);
+    exec(successCallback, errorCallback, "AirwatchAce", "getInfo", []);
+};
+
+module.exports = new AirwatchAce();
